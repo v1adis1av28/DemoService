22e8bec8acca24bc9ca5947e63c1a2ab0d7b458a
V1adislav28 <vladislovekrpzv@yandex.ru>
2025-08-10 15:48:31 +0300

    add gracefull shutdown

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"demo/internal/app"
+	"demo/internal/database"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
+
+	db := database.NewDB("postgres://postgres:postgres@db:5432/advertisements?sslmode=disable")
+	app := app.NewApp(db)
+
+	go func() {
+		app.MustStart()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	app.Stop()
+	log.Fatal("Gracefully stopped")
+
 }
---
+++ app/internal/app/app.go
@@ -0,0 +1,63 @@
+package app
+
+import (
+	"demo/internal/database"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	DB     *database.DB
+	Router *gin.Engine
+}
+
+func NewApp(db *database.DB) *App {
+	return &App{
+		DB:     db,
+		Router: gin.Default(),
+	}
+}
+
+func (a *App) MustStart() {
+	a.Router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (app *App) Run() error {
+
+	if err := app.SetupRoutes(); err != nil {
+		log.Fatal("Failed to setup server routes", "error", err)
+		return err
+	}
+
+	if err := app.Router.Run(); err != nil {
+		log.Fatal("Failed to start server", "error", err)
+		return err
+	}
+	return nil
+}
+
+func (app *App) SetupRoutes() error {
+	app.Router.POST("/api/advertisement")
+	app.Router.GET("/api/advertisements")
+	return nil
+}
+
+func (app *App) Stop() {
+	log.Fatal("stoping backend service")
+}
