44452c13bea6c9ef38772df65617bf3c6f0fb4cb
Vladislav <122610738+v1adis1av28@users.noreply.github.com>
2025-08-13 19:57:17 +0300

    Working version of demo service (#1)
    * add structure folders + add services kafka,db,redis + created dockerfile/docker-copmose
    
    * add migrations and schemas for project
    
    * add unique constraint on order_uid
    
    * add gracefull shutdown
    
    * add send and getting messages from kafka
    
    * add repository + handler for orders creating on kafka queue
    
    * add handling one order + changed to showing dto
    
    * add caching
    
    * fix gracefully shutdown
    
    * add frontend
    
    * took out functionality of producer on separete service
    
    * move system var to config
    
    * add integration tests

---
+++ app/cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"demo/internal/app"
+	"demo/internal/cache"
+	"demo/internal/config"
+	"demo/internal/database"
+	"demo/internal/handlers"
+	"demo/internal/kafka"
+	"demo/internal/repository"
+	"demo/internal/service"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	cfg := config.GetConfig("config/dev.yml")
+
+	db := database.NewDB(cfg.Database.PostgresURL)
+	redisClient := cache.NewRedisClient(cfg.Redis.Address, cfg.Redis.Password)
+
+	orderRepository := repository.NewOrderRepository(db.DB_CONN)
+	orderService := service.NewOrderService(orderRepository, redisClient)
+	orderHandler := handlers.NewOrderHandler(orderService)
+
+	app := app.NewApp(db, orderHandler, cfg)
+
+	go func() {
+		kafka.NewKafka(&kafka.KafkaInfo{
+			BrokkerAddress: cfg.Kafka.BrokerAddress,
+			Topic:          cfg.Kafka.Topic,
+			GroupId:        cfg.Kafka.GroupId,
+		})
+	}()
+
+	go func() {
+		app.MustStart()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	app.Stop()
+	log.Println("Gracefully stopped")
+}
---
+++ app/internal/handlers/order.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"demo/internal/models"
+	"demo/internal/service"
+	"demo/internal/utils"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderHandler struct {
+	orderService *service.OrderService
+}
+
+func NewOrderHandler(or *service.OrderService) *OrderHandler {
+	return &OrderHandler{orderService: or}
+}
+
+func (oh *OrderHandler) HandleIncomingOrder(c *gin.Context) {
+	var order models.Order
+	err := c.ShouldBindJSON(&order)
+	if err != nil {
+		log.Printf("Error while binding json %s", err.Error())
+		return
+	}
+	if !utils.ValidateOrder(&order) {
+		log.Printf("Error on validating order with id %s", order.OrderUID)
+		return
+	}
+	err = oh.orderService.NewOrder(&order)
+	if err != nil {
+		log.Printf("Error on creating new order %s", err.Error())
+		return
+	}
+
+	log.Printf("Order was succesfully created, order_id: %s", order.OrderUID)
+
+}
+
+func (oh *OrderHandler) GetOrderById(c *gin.Context) {
+	uuid := c.Param("id")
+	if len(uuid) < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "bad request, param id should be greater than 1"})
+		return
+	}
+
+	start := time.Now()
+	order, err := oh.orderService.GetOrderByUUID(uuid)
+	elapsedTime := time.Now().Sub(start)
+	fmt.Printf("Elapsed time: %s\n", elapsedTime)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "order with that id not found!"})
+		return
+	}
+	orderDTO := models.OrderToDTO(order)
+	c.JSON(http.StatusOK, orderDTO)
+
+}
